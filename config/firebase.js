33059d0a032ecd1e7c9ded73048473ba38e5fb46
Mithilesh <97399904+Mithil2305@users.noreply.github.com>
2025-10-03 22:37:07 +0530

    refactor: clean up unused imports and code in firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,38 +1,3 @@
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-// import Constants from "expo-constants";
-// import { initializeApp } from "firebase/app";
-// import { getReactNativePersistence, initializeAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getFunctions } from "firebase/functions";
-
-// // Get env variables safely
-// const extra = Constants.expoConfig?.extra || {};
-
-// const firebaseConfig = {
-// 	apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || extra.FIREBASE_API_KEY,
-// 	authDomain:
-// 		process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || extra.FIREBASE_AUTH_DOMAIN,
-// 	projectId:
-// 		process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || extra.FIREBASE_PROJECT_ID,
-// 	storageBucket:
-// 		process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET ||
-// 		extra.FIREBASE_STORAGE_BUCKET,
-// 	messagingSenderId:
-// 		process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||
-// 		extra.FIREBASE_MESSAGING_SENDER_ID,
-// 	appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || extra.FIREBASE_APP_ID,
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-
-// export const db = getFirestore(app);
-// export const functions = getFunctions(app);
-// export const auth = initializeAuth(app, {
-// 	persistence: getReactNativePersistence(AsyncStorage),
-// });
-// export default app;
-
 
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
